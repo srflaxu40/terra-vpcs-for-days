f1eb483550b4b0762fe55a601e2f56330d0a43ff
John Knepper <jknepper@Johns-MacBook-Pro.local>
2017-12-06 10:37:53 -0800

    Added dns hostname

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -3,7 +3,7 @@
 resource "aws_vpc" "vpc" {
   cidr_block         = "10.1.0.0/16"
   enable_dns_support = true
-  enable_dns_support = true 
+  enable_dns_hostnames = true 
 
   tags {
     Name = "vpc-${var.environment}"
